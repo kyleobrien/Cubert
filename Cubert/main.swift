0ee51a496372e4da94958b539e33d1671fdf98c2
Kyle O'Brien <me@kyleobrien.net>
2015-07-22 22:00:16 -0400

    Initial Commit

---
+++ Cubert/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Cubert
+//
+//  Created by Kyle O'Brien on 2015.7.22.
+//  Copyright © 2015 Kyle O'Brien. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
